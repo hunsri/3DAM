7f40115226cfb53a919d0edb963712932306a6c2
hunsri <richard.hunstock@gmx.de>
2026-01-27 23:34:41 +0100

    adds basic UI for asset exchange bar

---
+++ scenes/subscenes/server_load_changer.gd
@@ -0,0 +1,17 @@
+extends PanelContainer
+
+var is_upload: bool = false 
+@export var download_container: PanelContainer
+@export var upload_container: PanelContainer
+
+func _on_mode_changer_button_pressed() -> void:
+	is_upload = !is_upload 
+	
+	if is_upload:
+		upload_container.visible = true
+		download_container.visible = false
+	else:
+		upload_container.visible = false
+		download_container.visible = true
+		
+	pass # Replace with function body.
