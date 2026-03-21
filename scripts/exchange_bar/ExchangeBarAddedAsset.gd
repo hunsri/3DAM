e74dd177551d0a86a92ce9712eaba02c136e7218
hunsri <richard.hunstock@gmx.de>
2026-03-21 20:31:55 +0100

    adds docs to ExchangeBar

--- scripts/exchange_bar/ExchangeBarAddedAsset.gd
+++ scripts/exchange_bar/ExchangeBarAddedAsset.gd
@@ -1,17 +1,29 @@
+## For visually representing an asset selected for upload/download in the [ServerExchangeBar].
+##
+## Holds a reference to the corresponding asset tile, which is used to reset the SELECTED status of the tile when the asset is removed from the bar.
+## Independently from the asset tile, it shows the name and preview image of the asset.
 class_name ExchangeBarAddedAsset extends PanelContainer
 
+## UI element for displaying the name of the asset
 @export var _asset_name: Label
+## UI element for displaying the preview image of the asset
 @export var _asset_texture: TextureRect
 
+## Reference to the corresponding asset tile
+## Used for example to reset the SELECTED status of the tile when it is removed from [ServerExchangeBar]
 var asset_tile: AbstractAssetTile
 
+## Not in use at the moment
 var percentage_loaded: float = 0
 
+## For setting displayed name in the UI element
 func set_asset_name(p_asset_name: String) -> void:
 	_asset_name.text = p_asset_name
 
+## For setting displayed preview image in the UI element
 func set_asset_image(texture: Texture2D) -> void:
 	_asset_texture.texture = texture
 
+## Getter for the displayed asset name, independent from the asset tile
 func get_asset_name() -> String:
 	return _asset_name.text
--- scripts/exchange_bar/ServerExchangeBar.gd
+++ scripts/exchange_bar/ServerExchangeBar.gd
@@ -1,14 +1,25 @@
+## For the [code]ServerExchangeBar.tscn[/code] scene used in [code]ServerAssetBar.tscn[/code]
+##
+## The ServerExchangeBar is responsible for managing the assets that are currently selected for upload/download in the ServerAssetBar.
+## It displays the selected assets and has a button that triggers the upload/download action in the [ServerExchangeManager].
 class_name ServerExchangeBar extends Panel
 
+## for inserting the visual asset references 
 const EXCHANGE_BAR_ADDED_ASSET = preload("uid://lmanvmaaobvi")
 
-@export var upload_button_container: PanelContainer
-@export var download_button_container: PanelContainer
-@export var help_panel: HBoxContainer
+@export var upload_button_container: PanelContainer ## upload button UI element
+@export var download_button_container: PanelContainer ## download button UI element
+@export var help_panel: HBoxContainer ## panel that contains the help text shown when no assets are selected for upload/download
 
+## The container that holds the currently selected assets for upload/download. Each child is an [ExchangeBarAddedAsset]
+## which contains a reference to the corresponding asset tile.
 @export var added_assets_container: HBoxContainer
+
+## Set on ready by [ServerExchangeManager], which references the [ExchangeBar] inside the [ServerAssetBar] instance.
 var _exchange_manager: ServerExchangeManager
 
+## Helper method to create an [ExchangeBarAddedAsset] [br]
+## Takes an AssetTile and creates an [ExchangeBarAddedAsset] for it, which then can be given to [method ServerExchangeBar.add_to_bar].
 static func create_exchange_bar_asset(p_asset_tile: AbstractAssetTile) -> ExchangeBarAddedAsset:
 	var ret : ExchangeBarAddedAsset = EXCHANGE_BAR_ADDED_ASSET.instantiate()
 	ret.asset_tile = p_asset_tile
@@ -18,6 +29,10 @@ static func create_exchange_bar_asset(p_asset_tile: AbstractAssetTile) -> Exchan
 		ret.set_asset_image(ImageTexture.create_from_image(image))
 	return ret
 
+## Adds the created [ExchangeBarAddedAsset] to the bar and sets the exchange mode in the [ServerExchangeManager] if this is the first asset added.
+## The type of the first added asset determines the exchange mode. [br][br]
+## [ServerAssetTile2D] -> Download mode [br]
+## [AssetTile2D] -> Upload mode
 func add_to_bar(added_asset: ExchangeBarAddedAsset) -> void:
 	added_assets_container.add_child(added_asset)
 	
@@ -35,7 +50,9 @@ func add_to_bar(added_asset: ExchangeBarAddedAsset) -> void:
 		help_panel.visible = false
 		upload_button_container.visible = true
 		download_button_container.visible = false
-		
+
+## Removes the given [ExchangeBarAddedAsset] from the bar and resets the exchange mode in the [ServerExchangeManager] if this was the last asset in the bar. [br]		
+## Also resets the SELECTED status of the corresponding asset tile
 func remove_from_bar(added_asset: ExchangeBarAddedAsset) -> void:
 	var tile: AbstractAssetTile = added_asset.asset_tile
 	var sub_logic: TileSubLogic = tile.tile_sub_logic
@@ -48,7 +65,8 @@ func remove_from_bar(added_asset: ExchangeBarAddedAsset) -> void:
 		help_panel.visible = true
 		upload_button_container.visible = false
 		download_button_container.visible = false
-	
+
+## Similiar to [method ServerExchangeBar.remove_from_bar], but for clearing the whole bar at once.
 func clear_bar() -> void:
 	for child in added_assets_container.get_children():
 		var tile: AbstractAssetTile = child.asset_tile
@@ -57,12 +75,15 @@ func clear_bar() -> void:
 			sub_logic.change_status(TileSubLogic.TileStatus.DEFAULT)
 		
 		child.queue_free()
-	
+
+## Sets the reference to the [ServerExchangeManager] which is needed to set the exchange mode and trigger the upload/download
 func set_server_exchange_manager(server_exchange_manager: ServerExchangeManager):
 	_exchange_manager = server_exchange_manager
 
+## Initiates the upload of the selected assets listed in the bar
 func _on_upload_button_pressed() -> void:
 	_exchange_manager.upload_selected_assets()
 
+## Initiates the download of the selected assets listedin the bar
 func _on_download_button_pressed() -> void:
 	_exchange_manager.download_selected_assets()
