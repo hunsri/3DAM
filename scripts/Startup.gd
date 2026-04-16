07717403bbb8becccff25d74a94280bbc035d1ea
hunsri <richard.hunstock@gmx.de>
2026-04-16 19:11:06 +0200

    adds docs to Startup

--- scripts/Startup.gd
+++ scripts/Startup.gd
@@ -1,14 +1,25 @@
+## Entry point of the software
+##
+## Loads the identity of the user, and creates one if it doesn't exist.[br][br]
+## [color=blue]Note:[/color] This script MUST be included in the main scene, that is loaded on startup
 class_name Startup extends Node
 
+## Node holding the LocationSelector scene
+## New views are opened by adding them as a child to this node
 @export var SCENE_NODE: Node
 
+## Path to the file storing the identity of the user
 const ID_FILE_PATH := "user://identity.ini"
 
+## on ready, the identity of the user is loaded, and if it doesn't exist, a new one is created.
+## Initiates the scene with local asset view 
 func _ready() -> void:
 	_create_identity_if_not_exist()
 	
 	SCENE_NODE.add_child(Location_Handler.LOCAL_VIEW_UI.instantiate())
 
+## Static function to load the identity of the user from the file specified in [constant ID_FILE_PATH] [br]
+## Returns the uuid of the user as a string, or an empty string if loading fails
 static func load_identity_uuid() -> String:
 	var ret = ""
 	var config := ConfigFile.new()
@@ -17,7 +28,8 @@ static func load_identity_uuid() -> String:
 		ret = config.get_value("user", "uuid", "")
 	return ret
 
-func _create_identity_if_not_exist() -> void:
+## Static function to create a new identity for the user if it doesn't exist
+static func _create_identity_if_not_exist() -> void:
 	var config := ConfigFile.new()
 	var error = config.load(ID_FILE_PATH)
 	if error == ERR_FILE_NOT_FOUND:
