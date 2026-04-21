dd547ebce3c79e77f5139d375aa484ed93a45eb7
hunsri <richard.hunstock@gmx.de>
2026-04-21 20:50:18 +0200

    adds docs to ServerInfo

--- scripts/infos/ServerInfo.gd
+++ scripts/infos/ServerInfo.gd
@@ -1,3 +1,7 @@
+## Represents a server in the server list.
+##
+## Contains information about the server.
+## At the moment, only holds the address of the server as identfication
 class_name ServerInfo extends Control
 
-var address: String
+var address: String	    ## the address of the server, including the port
