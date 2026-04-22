6f0829d6ce6c60a0fcaa497e2d4d8ba7b942311a
hunsri <richard.hunstock@gmx.de>
2026-04-22 22:42:16 +0200

    Adds docs to ZipUtils

--- scripts/utils/ZipUtils.gd
+++ scripts/utils/ZipUtils.gd
@@ -1,14 +1,14 @@
-extends Object
-class_name ZipUtils
-## A class to helps handling zip archives
+## Class to help handling zip archives
 ##
 ## The provided functions are intended for compressing and decompressing assets
 ## in the preparation of the exchange of data with asset-servers
+class_name ZipUtils extends Object
 
-const TEMP_ZIP_DIR = "user://temp/upload_cache"
-const STANDARD_ARCHIVE_NAME: String = "assets.zip"
+const TEMP_ZIP_DIR = "user://temp/upload_cache"		## directory for storing created zip-archives
+const STANDARD_ARCHIVE_NAME: String = "assets.zip"	## name for the created zip-archives
 
-# Read a single file from a ZIP archive.
+## Read a single file from a ZIP archive. [br][br]
+## Returns the content of the file as a PackedByteArray, or an empty PackedByteArray if the file could not be read.
 static func read_zip_file(path_to_archive: String, path_in_archive:String):
 	var reader = ZIPReader.new()
 	var err = reader.open(path_to_archive)
@@ -18,7 +18,7 @@ static func read_zip_file(path_to_archive: String, path_in_archive:String):
 	reader.close()
 	return res
 
-# Extract all files from a ZIP archive, preserving the directories within.
+## Extract all files from a ZIP archive, preserving the directories within.
 static func extract_all_from_zip(path_to_archive: String, path_to_destination: String):
 	var reader = ZIPReader.new()
 	reader.open(path_to_archive)
@@ -49,7 +49,7 @@ static func extract_all_from_zip(path_to_archive: String, path_to_destination: S
 ## Creates a zip-archive of a given asset to be used to upload it to an asset-server [br]
 ## The zip-archive is placed under [constant ZipUtils.TEMP_ZIP_DIR] [br]
 ## 
-## If asset is part of a package, the asset_info of the respective package_version must be provided
+## If asset is part of a package, the [code]asset_info[/code] of the respective package_version must be provided
 ## Returns the path to the created zip-archive
 static func create_zip_from_asset_info(asset_info: AssetInfo) -> String:
 	var target = asset_info.get_path_to_local_asset()
@@ -61,7 +61,7 @@ static func create_zip_from_asset_info(asset_info: AssetInfo) -> String:
 	return create_zip_archive(target, destination, asset_info)
 
 ## Creates a zip archive [br]
-## Created archive will always be of the name [constant ZipUtils.STANDARD_ARCHIVE_NAME] [br]
+## Created archive will always be of the name [constant ZipUtils.STANDARD_ARCHIVE_NAME] [br][br]
 ## Returns the path to the created archive
 static func create_zip_archive(path_to_target: String, path_to_destination: String, asset_info: AssetInfo) -> String:
 	var zipper := ZIPPacker.new()
@@ -87,14 +87,22 @@ static func create_zip_archive(path_to_target: String, path_to_destination: Stri
 	zipper.close()
 	return ProjectSettings.globalize_path(zip_path)
 
+## Helper functions for creating zip-archives, for adding files to the archive [br][br]
+## [param zipper] The zipper to add the file to [br]
+## [param source_file_path] The path to the file to add [br]
+## [param target_path_in_zip] The path within the zip archive to place the file at, left empty the file is placed at the archive root
 static func _add_file(zipper: ZIPPacker, source_file_path: String, target_path_in_zip: String = ""):
 	if target_path_in_zip == "":
-		target_path_in_zip = source_file_path.get_file()
+		target_path_in_zip = source_file_path.get_file() # just taking the file name, which places it at the archive root
 	
 	zipper.start_file(target_path_in_zip)
 	zipper.write_file(_get_packed_bytes(source_file_path))
 	zipper.close_file()
 
+## Helper function for creating zip-archives, for adding directories to the archive recursively [br][br]
+## [param zipper] The zipper to add the directory to [br]
+## [param root] The root directory of the target directory [br]
+## [param current] The current directory to add, which is used for the recursive calls
 static func _add_dir_recursive(zipper: ZIPPacker, root: String, current: String) -> void:
 	var dir := DirAccess.open(current)
 	if dir == null:
@@ -113,6 +121,9 @@ static func _add_dir_recursive(zipper: ZIPPacker, root: String, current: String)
 	for subdir in dir.get_directories():
 		_add_dir_recursive(zipper, root, current.path_join(subdir))
 
+## Helper functions for creating zip-archives, for reading files to be added to the archive [br][br]
+## [param path] The path to the file to read [br]
+## Returns the content of the file as a PackedByteArray, or an empty PackedByteArray if the file could not be read.
 static func _get_packed_bytes(path: String) -> PackedByteArray:
 	var fa := FileAccess.open(path, FileAccess.READ)
 	if fa == null:
@@ -122,6 +133,9 @@ static func _get_packed_bytes(path: String) -> PackedByteArray:
 	fa.close()
 	return bytes
 
+## Helper function to make ensure the location for the zip archive exists [br][br]
+## [param file_path] The path to the zip archive to be created [br][br]
+## Returns true if the directory was created successfully or already exists, false otherwise
 static func _create_path(file_path: String) -> bool:
 	var dir_path := ProjectSettings.globalize_path(file_path)
 	var root := DirAccess.open("")  # required to call make_dir_recursive
